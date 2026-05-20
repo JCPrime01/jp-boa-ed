78b558d5ee7b3bb06a7f38e1ce1689c2dc56a8f0
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-20 12:04:41 -0300

    chore: update shareCode to 6T7X9721G0W

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=2OGH3P4TOP7&afp=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/hncv0ue6?shareCode=6T7X9721G0W&afp=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
